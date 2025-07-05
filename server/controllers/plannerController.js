c7418e623d235f67e8a752e390dce784702740d1
farhanpavel <farhanpavel3@gmail.com>
2025-07-05 21:57:43 +0600

    bug fix

--- server/controllers/plannerController.js
+++ server/controllers/plannerController.js
@@ -3,7 +3,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const Planner = require("../models/plannerSchema");
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-2.0-pro-exp-02-05" });
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 const getGeneratedPlan = async (req, res) => {
   try {
